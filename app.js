5e6065389b4b5f6bc41f3ea92ff0f2bbed1a2bd2
qsadozai <125207185+qsadozai@users.noreply.github.com>
2025-01-06 16:15:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/*     <nav class="nav-tg">
+      Logo
+
+      <input class="input-tg" type="text" placeholder="search" />
+
+      <a class="a-tg" href="">News</a>
+      <a class="a-tg" href="">About Us</a>
+      <a class="a-tg" href="">Bookmark</a>
+
+      <hr />
+    </nav>
+ */
